0b6f3c13dc4c483a19f19b3c228a8465ce910844
solo <soloman.err@.com>
2023-10-02 18:33:26 +0600

    wraply server launched

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 2000;
+require('dotenv').config();
+const cors = require('cors');
+app.use(cors());
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wndd9z6.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect((error) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+    });
+
+    // DATABASE-COLLECTIONS:||------------------------>>>>
+    const productsCollection = client.db('wraply').collection('products');
+
+    // PRODUCTS:||------------------------>>>>
+    app.get('/products', async (req, res) => {
+      try {
+        const products = await productsCollection.find({}).toArray();
+        res.json(products);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+    });
+
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+app.get('/', (req, res) => {
+  res.send('Wraply Engine');
+});
+
+app.listen(port, () => {
+  console.log(`Wraply running on port ${port}`);
+});
